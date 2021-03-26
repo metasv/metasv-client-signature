a7a3a09b4e38ea2dd5c17912ef98c49e6e82e44e
KaKeimei <hqm237594742@gmail.com>
2021-03-26 20:11:18 +0900

    add more log

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ const path = "/block/000000000000000007dded8e2a733c654a006520409cdb0d6cdf642a132
 const timestamp = Date.now()
 const nonce = Math.random().toString().substr(2, 10);
 const message = path + '_' + timestamp + '_' + nonce
-console.log(message)
-console.log(publicKey.toHex())
+console.log("Message: " + message)
+console.log("Pubkey: " + publicKey.toHex())
 
 // generate signature
 const hash = bsv.Hash.sha256(Buffer.from(message))
 const sig = bsv.Ecdsa.sign(hash, keyPair)
 const sigEncoded = sig.toBuffer().toString("base64")
-console.log(sigEncoded)
+console.log("Signature: " + sigEncoded)
 
 // verify the signature is valid
-console.log(bsv.Ecdsa.verify(hash, bsv.Sig.fromBuffer(Buffer.from(sigEncoded, "base64")), publicKey))
+console.log("Valid: " + bsv.Ecdsa.verify(hash, bsv.Sig.fromBuffer(Buffer.from(sigEncoded, "base64")), publicKey))
 
 // print send request
 axios.interceptors.request.use(request => {
-    console.log('Starting Request', JSON.stringify(request, null, 2))
+    console.log('Request: ', JSON.stringify(request, null, 2))
     return request
 })
 
@@ -40,6 +40,6 @@ axios.get(domain + path, {
         'MetaSV-Signature': sigEncoded
     }
 }).then((response => {
-    console.log(response.data);
+    console.log('Response: ', response.data);
 }))
 
